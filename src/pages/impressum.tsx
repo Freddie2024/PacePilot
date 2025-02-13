41167a532338336076a083aaf9c4be0a40ce7d22
FRIDA LEMKE <frida.lemke@gmx.de>
2025-02-13 17:15:30 +0100

    clear cache

---
+++ src/pages/impressum.tsx
@@ -0,0 +1,15 @@
+export default function Impressum {
+    return (
+        <p>Image by 
+            <a href="https://pixabay.com/users/surprising_snapshots-11873433/"
+            >
+            Surprising_SnapShots
+            </a>
+            from 
+            <a href="https://pixabay.com/"
+            >
+            Pixabay
+            </a>
+        </p>
+    )
+}
