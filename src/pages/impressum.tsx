f97d68762768574f53f5901740e83b3349309ecf
FRIDA LEMKE <frida.lemke@gmx.de>
2025-02-14 16:10:01 +0100

    add impressum

--- src/pages/impressum.tsx
+++ src/pages/impressum.tsx
@@ -1,5 +1,7 @@
 export default function Impressum() {
     return (
+        <>
+        <p>Trainingspläne aus DAS GROSSE LAUFBUCH von Herbert Steffny</p>
         <p>Image by 
             <a href="https://pixabay.com/users/surprising_snapshots-11873433/"
             >
@@ -11,5 +13,6 @@ export default function Impressum() {
             Pixabay
             </a>
         </p>
+        </>
     )
 }
